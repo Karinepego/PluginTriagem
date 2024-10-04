1f00f564f6e6c767b1ff0a9d3493385a77c4787f
Karinepego <karinepegoribeiro@gmail.com>
2024-10-04 15:07:51 -0300

    modificando a funcionalidade do botao de favotitar

--- wp-content/plugins/favorite-posts-plugin/favorite-posts.js
+++ wp-content/plugins/favorite-posts-plugin/favorite-posts.js
@@ -5,48 +5,21 @@ jQuery(document).ready(function($) {
 
         $.ajax({
             method: 'POST',
-            url: wpApiSettings.root + 'favorite-posts/v1/toggle/' + postId, // URL da API
+            url: favoriteData.ajax_url + postId, // Corrigido para usar favoriteData.ajax_url
             beforeSend: function(xhr) {
-                xhr.setRequestHeader('X-WP-Nonce', wpApiSettings.nonce); // Adiciona o nonce para segurança
+                xhr.setRequestHeader('X-WP-Nonce', favoriteData.nonce); // Adiciona o nonce para segurança
             },
             success: function(response) {
                 if (response.status === 'favorited') {
                     button.text('Desfavoritar'); // Atualiza o texto do botão
-                } else {
-                    button.text('Favoritar'); // Atualiza o texto do botão
-                }
-            },
-            error: function(error) {
-                console.log('Erro:', error); // Loga qualquer erro
-            }
-        });
-    });
-});
-
-add_action('wp_enqueue_scripts', 'enqueue_favorite_script');
-
-jQuery(document).ready(function($) {
-    $('.favorite-button').click(function() {
-        var postId = $(this).data('post-id');
-        var button = $(this);
-
-        $.ajax({
-            method: 'POST',
-            url: wpApiSettings.root + 'favorite-posts/v1/toggle/' + postId,
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader('X-WP-Nonce', wpApiSettings.nonce);
-            },
-            success: function(response) {
-                if (response.status === 'favorited') {
-                    button.text('Desfavoritar');
                     button.removeClass('favoritar').addClass('desfavoritar'); // Altera a classe
                 } else {
-                    button.text('Favoritar');
+                    button.text('Favoritar'); // Atualiza o texto do botão
                     button.removeClass('desfavoritar').addClass('favoritar'); // Altera a classe
                 }
             },
             error: function(error) {
-                console.log('Erro:', error);
+                console.log('Erro:', error); // Loga qualquer erro
             }
         });
     });
